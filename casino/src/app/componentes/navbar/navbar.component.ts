3591126cb5494575283673b34a41be87fb934fb2
Juangaca02 <juanangarciacastro@gmail.com>
2024-02-13 20:23:21 +0100

    fix casino

--- casino/src/app/componentes/navbar/navbar.component.ts
+++ casino/src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isNavbarBlurred: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Lógica para verificar la posición del scroll y aplicar la clase de borrosidad según sea necesario
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isNavbarBlurred = scrollPosition > 0; // Aplicar la borrosidad si la posición del scroll es mayor que 0
+  }
 }
